79357d6b18f66eb2eed81d381b434395bfd0f337
Choria <2963482580qq.com>
2026-01-29 15:34:33 +0800

    linked_list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -5,11 +5,11 @@
 struct Node {
     int data;
     struct Node* next;
-}
+};
 
 //创建节点
 struct Node* create_new_node(int data){
-   struct Node* new_node = (struct *Node)malloc(sizeof(struct Node*));
+   struct Node* new_node = (struct Node*)malloc(sizeof(struct Node*));
     new_node->data = data;
     new_node->next = NULL;
 return new_node;
@@ -25,18 +25,56 @@ void append(struct Node** head,int data){
    }
 //如果不是头节点, 那么就从头节点遍历寻找链表的最后一个节点;
 struct Node* current = *head;
-  while(current->next != NULL;){
+  while(current->next != NULL){
     current = current->next;
   }
 current->next = new_node;
 } 
 
 
-//输出一个链表:
+//输出一个链表:从头到尾循环遍历一遍链表
+void print_node(struct Node* head){
+ 
+   struct Node* current = head;
+    
+   while(current != NULL){
+       printf("%d -> ",current->data);
+       current = current->next;
+   }
 
+  printf("NULL\n");
+}
+
+
+//释放内存
+void free_list(struct Node* head){
+    struct Node* current;
+    struct Node* next;
+    
+     while(head != NULL){
+         current = head;
+         next = current->next;
+         free(current);
+         current = next;
+     }
+
+}
 
 //简单链表实现
 int main(){
+
+   //初始化空列表
+ struct Node* head = NULL;
  
+append(&head,10);
+append(&head,20); 
+append(&head,30);
+
+//打印结果
+printf("链表内容: ");
+print_node(head);
+
+//释放内存
+free_list(head);
    return 0;
 }
